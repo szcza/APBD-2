8c7c8cfce3d190f1eec94c6a074ac618a2e81e96
s27538 <s27538@pjwstk.edu.pl>
2024-03-18 13:26:10 +0100

    Dodano Klase Kontener.cs

---
+++ APBD-2/APBD-2/Kontener.cs
@@ -0,0 +1,26 @@
+﻿namespace APBD_2;
+
+public class Kontener
+{
+    private int mass;
+    private int height;
+    //properties getter i setter - autoproperty
+    public string Depth { get; set; }
+    //Full property
+    private double _weight;
+
+    public double Weight
+    {
+        get { return _weight; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Weight cannot be less than zero");
+            }
+            _weight = value;
+        }
+    }
+    
+    
+}

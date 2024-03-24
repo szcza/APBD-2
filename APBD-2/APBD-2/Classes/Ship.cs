75802eaae2dbd726f90c15bebbe6ee60603e4608
s27538 <s27538@pjwstk.edu.pl>
2024-03-24 21:44:25 +0100

    HUH

--- APBD-2/APBD-2/Classes/Ship.cs
+++ APBD-2/APBD-2/Classes/Ship.cs
@@ -2,5 +2,16 @@
 
 public class Ship
 {
-    
+    public List<Container> Containers;
+    public double speed;
+    public int maxContainers;
+    public double maxWeight;
+
+    public Ship(List<Container> containers, double speed, int maxContainers, double maxWeight)
+    {
+        Containers = containers;
+        this.speed = speed;
+        this.maxContainers = maxContainers;
+        this.maxWeight = maxWeight;
+    }
 }
---
+++ APBD-2/APBD-2/Classes/ShippingManager.cs
@@ -0,0 +1,23 @@
+﻿namespace APBD_2;
+
+public class ShippingManager
+{
+    
+    public LiquidContainer LiquidContainerCreator(int currentLoad, int height, int containerWeight, int depth, int maxLoad, bool isHazardous)
+    {
+        LiquidContainer lq = new LiquidContainer()
+        string serialNumber="KON-L"
+    }
+
+    public GasContainer GasContainerCreator(double currentLoad, int height, int containerWeight, int depth, int maxLoad, int pressure)
+    {
+        string serialNumber=
+    }
+
+    public CoolingContainer CoolingContainerCreator(double currentLoad, int height, int containerWeight, int depth, int maxLoad, List<Product> products, double containerTemperature)
+    {
+        string serialNumber=
+    }
+    
+    
+}
